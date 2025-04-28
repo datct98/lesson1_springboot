b7faf35b1fb45b70cf55d79c262c71d3d30bc74f
datct15 <kiwisun1998@gmail.com>
2025-04-28 20:42:35 +0700

    init project

--- src/main/java/org/example/lesson1/Lesson1Application.java
+++ src/main/java/org/example/lesson1/Lesson1Application.java
@@ -1,19 +1,37 @@
 package org.example.lesson1;
 
-import org.example.lesson1.view.Menu;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.example.lesson1.entities.Book;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Lesson1Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello World!");
-        while (true){
+        /*while (true){
             Menu menu = new Menu();
             menu.displayMenu();
+        }*/
+        Gson gson = new Gson();
+        try {
+            FileReader reader = new FileReader("read_file.json");
+            Type typeBook = new TypeToken<ArrayList<Book>>() {}.getType();
+            List<Book> books = gson.fromJson(reader, typeBook);
+            System.out.println(books);
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
         }
+
 
     }
 
